db04e334f134c955bca4a575fdd0e573008db06c
tamerelmenoufi <tamer@mohatron.com.br>
2025-04-17 18:31:39 -0400

    Atualizações

--- lib/connection.php
+++ lib/connection.php
@@ -5,7 +5,7 @@ if ($_SERVER['HTTP_HOST'] === 'localhost' || $_SERVER['HTTP_HOST'] === '10.0.0.1
     define('DB_PASSWORD', '');
     define('DB_DATABASE', 'politica');
 } else {
-    define('DB_HOST', '100.42.189.93');
+    define('DB_HOST', '100.42.189.93:3390');
     define('DB_USERNAME', 'root');
     define('DB_PASSWORD', 'S3nh@sb@nc0');
     define('DB_DATABASE', 'politica');
