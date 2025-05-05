823e121c428c04bdbbccb8e75e9a62028183dca0
Tamer Elmenoufi <tamer.menoufi@gmail.com>
2025-05-05 17:01:05 +0300

    first commit

--- painel/sidebar.php
+++ painel/sidebar.php
@@ -3,9 +3,9 @@
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
         <div class="sidebar-brand-icon rotate-n-15">
-            <i class="fas fa-laugh-wink"></i>
+            <i class="fa-solid fa-brain"></i>
         </div>
-        <div class="sidebar-brand-text mx-3" title="Sistema de Gestão Política">SGP</div>
+        <div class="sidebar-brand-text mx-3" title="Sistema de Gestão Política">CÉREBRO</div>
     </a>
 
     <!-- Divider -->
